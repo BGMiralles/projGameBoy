a05705b5ef2bfd148a7791edee2d720712a21179
Borja <pintocorreas@gmail.com>
2023-09-24 19:54:35 +0200

    Implementacion de audios a los botones

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const opciones = ["turnOn", "turnOnSquirt", "turnOnMario"]
+let audio = new Audio();
 
 function turnScreenOn(){
+    let audio = document.getElementById("miAudioStart");
     let screen = document.getElementById("screenOn");
     for(var i =0; i < opciones.length; i++){
         if (opciones[i] == "turnOn" || opciones[i] == "turnOnSquirt" || opciones[i] == "turnOnMario"){
@@ -8,21 +10,23 @@ function turnScreenOn(){
         }
     }
     screen.classList.add("turnOn");
-    let audio = new Audio();
     audio.play();
 }
 
 function turnScreenOnSquirt(){
+    let audio = document.getElementById("miAudioA");
     let screen = document.getElementById("screenOn");
     for(var i =0; i < opciones.length; i++){
         if (opciones[i] == "turnOn" || opciones[i] == "turnOnSquirt" || opciones[i] == "turnOnMario"){
             screen.classList.remove(`${opciones[i]}`);
         }
-}
+    }
     screen.classList.add("turnOnSquirt");
+    audio.play();
 }
 
 function turnScreenOnMario(){
+    let audio = document.getElementById("miAudioB");
     let screen = document.getElementById("screenOn");
     for(var i =0; i < opciones.length; i++){
         if (opciones[i] == "turnOn" || opciones[i] == "turnOnSquirt" || opciones[i] == "turnOnMario"){
@@ -30,6 +34,7 @@ function turnScreenOnMario(){
         }
 }
     screen.classList.add("turnOnMario");
+    audio.play();
 }
 
 function turnScreenOff(){
