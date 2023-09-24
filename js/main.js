5bcb52a863fe8fbb77913704a86e83045b40cba3
Borja <pintocorreas@gmail.com>
2023-09-24 18:36:01 +0200

    Funcionalidad boton start, 'B' y mejora del archivo js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
- function turnScreenOff(){
-     let main = document.getElementById("screenOn");
-     main.classList.remove("turnOn");
-    
+const opciones = ["turnOn", "turnOn1"]
+
+function turnScreenOn(){
+    let screen = document.getElementById("screenOn");
+    for(var i =0; i < opciones.length; i++){
+        if (opciones[i] == "turnOn" || opciones[i] == "turnOn1"){
+            screen.classList.remove(`${opciones[i]}`);
+        }
+    }
+    screen.classList.add("turnOn");
+    let audio = new Audio();
+    audio.src = "../sound/gameboy-1.mp3";
+    audio.play();
 }
+
+function turnScreenOn1(){
+    let screen = document.getElementById("screenOn");
+    for(var i =0; i < opciones.length; i++){
+        if (opciones[i] == "turnOn" || opciones[i] == "turnOn1"){
+            screen.classList.remove(`${opciones[i]}`);
+        }
+}
+    screen.classList.add("turnOn1");
+}
+
+function turnScreenOff(){
+    let main = document.getElementById("screenOn");
+    for(var i =0; i < opciones.length; i++){
+        if (opciones[i] == "turnOn" || opciones[i] == "turnOn1"){
+            main.classList.remove(`${opciones[i]}`);
+        }
+}}
