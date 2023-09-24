2998c6507cb74b48d82e8b5b359da4a105f104b7
Borja <pintocorreas@gmail.com>
2023-09-24 18:47:37 +0200

    Funcionalidad boton 'B'

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-const opciones = ["turnOn", "turnOn1"]
+const opciones = ["turnOn", "turnOnSquirt", "turnOnMario"]
 
 function turnScreenOn(){
     let screen = document.getElementById("screenOn");
     for(var i =0; i < opciones.length; i++){
-        if (opciones[i] == "turnOn" || opciones[i] == "turnOn1"){
+        if (opciones[i] == "turnOn" || opciones[i] == "turnOnSquirt" || opciones[i] == "turnOnMario"){
             screen.classList.remove(`${opciones[i]}`);
         }
     }
@@ -13,20 +13,30 @@ function turnScreenOn(){
     audio.play();
 }
 
-function turnScreenOn1(){
+function turnScreenOnSquirt(){
     let screen = document.getElementById("screenOn");
     for(var i =0; i < opciones.length; i++){
-        if (opciones[i] == "turnOn" || opciones[i] == "turnOn1"){
+        if (opciones[i] == "turnOn" || opciones[i] == "turnOnSquirt" || opciones[i] == "turnOnMario"){
             screen.classList.remove(`${opciones[i]}`);
         }
 }
-    screen.classList.add("turnOn1");
+    screen.classList.add("turnOnSquirt");
+}
+
+function turnScreenOnMario(){
+    let screen = document.getElementById("screenOn");
+    for(var i =0; i < opciones.length; i++){
+        if (opciones[i] == "turnOn" || opciones[i] == "turnOnSquirt" || opciones[i] == "turnOnMario"){
+            screen.classList.remove(`${opciones[i]}`);
+        }
+}
+    screen.classList.add("turnOnMario");
 }
 
 function turnScreenOff(){
     let main = document.getElementById("screenOn");
     for(var i =0; i < opciones.length; i++){
-        if (opciones[i] == "turnOn" || opciones[i] == "turnOn1"){
+        if (opciones[i] == "turnOn" || opciones[i] == "turnOnSquirt" || opciones[i] == "turnOnMario"){
             main.classList.remove(`${opciones[i]}`);
         }
 }}
